c0d47793b50152dca96bb802daa4cf43723435ef
d33raj <97349512+d33raj@users.noreply.github.com>
2024-10-09 19:43:13 +0530

    Add files via upload

---
+++ MovieLibrary/Exceptions/MovieListEmptyException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieLibrary.Exceptions
+{
+    public class MovieListEmptyException : Exception
+    {
+        public MovieListEmptyException(string message) : base(message)
+        { }
+    }
+}
