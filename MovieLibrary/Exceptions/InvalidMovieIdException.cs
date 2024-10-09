671e76a615f896fdb11c1e0d27297e6f55a3a30c
d33raj <97349512+d33raj@users.noreply.github.com>
2024-10-09 19:42:32 +0530

    Create InvalidMovieIdException.cs

---
+++ MovieLibrary/Exceptions/InvalidMovieIdException.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieLibrary.Exceptions
+{
+    public class InvalidMovieIdException : Exception
+    {
+        public InvalidMovieIdException(string message) : base(message)
+        { }
+    }
+}
